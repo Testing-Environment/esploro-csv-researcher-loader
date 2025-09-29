ab8a3531518950beaca346ae93f139195027d0c1
fxaiz <adfaizer@gmail.com>
2025-09-29 19:55:52 +0800

    fixed the env URL in config.json and added a log line in logs.txt

--- cloudapp/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  firekey: 'l8xx9a0da0b9b183434fbdc5df57292da9e6' // Replace with your actual production key
-};
--- cloudapp/src/environments/environments.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  firekey: 'l8xx9a0da0b9b183434fbdc5df57292da9e6' // Replace with your actual dev key
-};
